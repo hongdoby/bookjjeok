e6e794f8a312ac3b6bad4fae57ab2db92522acf7
hongdoby <rudfkr9412@gmail.com>
2026-04-22 18:28:58 +0900

    Add independent test environments for vpc1, vpc2, vpc3

---
+++ environments/test_vpc1/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc1_cidr" {
+  description = "VPC1 (클라우드 환경) 네트워크 CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc1_prefix" {
+  description = "VPC1 리소스에 사용할 접두사"
+  type        = string
+  default     = "bookjjeok-test-vpc1"
+}
+
+variable "vpc1_cluster_name" {
+  description = "VPC1에 배포될 EKS 클러스터 이름"
+  type        = string
+  default     = "bookjjeok-test-eks-cluster"
+}
